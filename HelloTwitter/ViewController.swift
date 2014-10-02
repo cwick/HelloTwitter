7df09222ead930101be6c4f5f9eac3a759da1efd
Carmen Wick <CarmenWick@gmail.com>
2014-10-02 16:33:50 -0700

    Initial Commit

---
+++ HelloTwitter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloTwitter
+//
+//  Created by Carmen Wick on 10/2/14.
+//  Copyright (c) 2014 Carmen Wick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
