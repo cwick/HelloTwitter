4545c27c25179f0e64a62bc7854079b502c0a718
Carmen Wick <CarmenWick@gmail.com>
2014-10-06 18:49:51 -0700

    Start adding settings

---
+++ HelloTwitter/SettingsTableViewController.swift
@@ -0,0 +1,33 @@
+//
+//  Created by Carmen Wick on 10/6/14.
+//  Copyright (c) 2014 Carmen Wick. All rights reserved.
+//
+
+import UIKit
+
+class SettingsTableViewController: UITableViewController, UITextFieldDelegate {
+  @IBOutlet weak var appSecretLabel: UILabel!
+  @IBOutlet weak var appSecretValue: UITextField!
+  
+  @IBOutlet weak var appKeyLabel: UILabel!
+  @IBOutlet weak var appKeyValue: UITextField!
+  
+  @IBAction func labelTapped(sender: UITapGestureRecognizer) {
+    (sender.view?.superview?.subviews[1] as UIResponder).becomeFirstResponder()
+  }
+  
+  func textFieldShouldReturn(textField: UITextField) -> Bool {
+    if textField == appKeyValue {
+      appSecretValue.becomeFirstResponder()
+    }
+    else {
+      textField.resignFirstResponder()
+    }
+    
+    return true
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
