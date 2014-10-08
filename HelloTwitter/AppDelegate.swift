f9b6e86d22f88c7a6c01e079c9eb1a4a9c785820
Carmen Wick <CarmenWick@gmail.com>
2014-10-07 19:09:13 -0700

    Register defaults

--- HelloTwitter/AppDelegate.swift
+++ HelloTwitter/AppDelegate.swift
@@ -17,8 +17,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   
   func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
     // Override point for customization after application launch.
-    
+
     App.managedContext = managedObjectContext
+    App.defaults.registerDefaults(["app_key": "", "app_secret": ""])
     
     return true
   }
